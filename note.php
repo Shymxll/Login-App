8d9b1e59ec2ccad13fe565cc2636d87f7aa390dd
shamkhal <121178638+Shymxll@users.noreply.github.com>
2023-04-17 13:06:05 +0400

    added note table

--- note.php
+++ note.php
@@ -1,7 +1,7 @@
 <?php
 
 session_start();
-
+include_once("connection.php");
 //get id parameter
 $userId = base64_decode($_SESSION['id']);
 
@@ -11,10 +11,11 @@ if(isset($_POST["noteAdd"])){
   
 
   if($_POST["noteText"] != ""){
-      include_once("connection.php");
+     
       //insert query note table query, use user_id parameter, use noteText parameter
       $query = "INSERT INTO notes (user_id,note) VALUES ('$userId','$noteText')";
       //execute query
+      
       $result = mysqli_query($connection,$query);
       //fetch result
       if ($result) {
@@ -24,13 +25,17 @@ if(isset($_POST["noteAdd"])){
        ';
        
       } else {
-          
+        echo '<div class="alert alert-success" role="alert">
+        Unsuccess
+        </div>
+       '; 
       }
   }
-  else{
-    
-  }
- 
+  
+  
+
+
+  
 }
 ?>
 
@@ -46,13 +51,46 @@ if(isset($_POST["noteAdd"])){
         <div class="mb-3">   
                         
             <div class="mb-3">
-              <form method = "POST" action="">
+              <form method = "POST" action="note.php">
             <label for="exampleFormControlTextarea1" name="note" class="form-label">Example textarea</label>
             <input type="text" class="form-control" name="noteText" id="exampleFormControlTextarea1" rows="3"></i>
            </div>
             <button type="submit" name="noteAdd" class="btn btn-primary">Submit</button>
-            </div>
+            
             </form>
+            </div>
+            <div class="container-sm ">
+            <table class="table">
+                <thead>
+                  <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Note</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <?php
+                     // get all notes from notes table
+                      $query = "SELECT * FROM notes";
+                      //execute query
+                      $result = mysqli_query($connection,$query);
+                      //fetch result
+                      if($result){
+                        while($row = mysqli_fetch_assoc($result)){
+                          echo "<tr>";
+                          echo "<td>".$row['id']."</td>";
+                          echo "<td>".$row['user_id']."</td>";
+                          echo "<td>".$row['note']."</td>";
+                          echo "</tr>";
+                        }
+                      }
+
+                   ?>
+                  </tr>
+                </tbody>
+              </table>
+
             </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
   </body>
